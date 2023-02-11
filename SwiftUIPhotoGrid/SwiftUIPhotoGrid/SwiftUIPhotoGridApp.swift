9b577faddd8b162e7ab8e10839bfddc91fa41096
David de la Puente <david.puente@marbleinteractive.com>
2023-02-11 17:06:36 +0100

    Add images

---
+++ SwiftUIPhotoGrid/SwiftUIPhotoGrid/SwiftUIPhotoGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPhotoGridApp.swift
+//  SwiftUIPhotoGrid
+//
+//  Created by Marble Interactive SL on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPhotoGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
